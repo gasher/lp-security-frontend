792f08cc9f6932205c3bd26d0cea61f097993414
Robert Vintila <robert.vintila@algotech.solutions>
2020-04-20 18:40:28 +0300

    Add env variable for api.

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   api: {
     // baseURL: 'https://blooming-forest-52131.herokuapp.com/',
-    baseURL: 'http://localhost:8000/',
+    baseURL: process.env.API_URL || 'http://localhost:8000/',
     timeout: 25000,
     headers: {
       common: {
