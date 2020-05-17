4311014ea11a49e1ac32da61e26bcd2a2707683b
Robert Vintila <robert.vintila@algotech.solutions>
2020-05-17 19:02:48 +0300

    Add copy config button.

--- src/components/entities/ConfigFile/index.js
+++ src/components/entities/ConfigFile/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const ConfigFile = props => {
-  return <pre>{props.content}</pre>;
+  const [copied, setCopied] = useState(false);
+
+  const copySnippet = () => {
+    const target = document.getElementById('config-file-content');
+
+    if (document.createRange) {
+      let range = document.createRange();
+      let select = window.getSelection();
+      range.selectNode(target);
+      select.removeAllRanges();
+      select.addRange(range);
+      document.execCommand('copy');
+      select.removeAllRanges();
+    } else {
+      let range = document.body.createTextRange();
+      range.moveToElementText(target);
+      range.select();
+      document.execCommand('copy');
+    }
+
+    return setCopied(true);
+  };
+  return (
+    <div>
+      <pre id="config-file-content">{props.content}</pre>
+      {document.queryCommandSupported('copy') && (
+        <div>
+          <Button onClick={copySnippet} variant="info">
+            Copy snippet
+          </Button>{' '}
+          {copied && <p>Content copied!</p>}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ConfigFile;
